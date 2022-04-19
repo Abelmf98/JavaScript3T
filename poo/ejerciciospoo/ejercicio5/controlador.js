d3968b1edc68d47996ba61c42dbecf5b283be17b
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-04-19 19:50:57 +0200

    5 actualizado sin terminar

--- poo/ejerciciospoo/ejercicio5/controlador.js
+++ poo/ejerciciospoo/ejercicio5/controlador.js
@@ -1,9 +1,14 @@
+import 'vista.js'
+
 'use strict'
 
-class Controlador{
 
+class Controlador{
+    constructor(vista=Vista, modelo=Modelo){
+        this.vista = vista
+        this.modelo = modelo
+    }
 }
 
 var objeto = new Controlador()
 
-var modelo = new Modelo()
--- poo/ejerciciospoo/ejercicio5/vista.js
+++ poo/ejerciciospoo/ejercicio5/vista.js
@@ -1,5 +1,8 @@
 'use strict'
 
 class Vista{
-    
+    mostrar(){
+        console.log('El sentido del universo es...')
+        return 0
+    }
 }
